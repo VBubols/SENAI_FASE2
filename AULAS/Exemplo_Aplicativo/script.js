b32e291489f25ef6d9340826fb465e5cb2518170
VITOR RAZIA BUBOLS <vitor_bubols@estudante.sesisenai.org.br>
2025-08-21 22:07:55 -0300

    [JS AND HTML] created new functions to add and remove users

---
+++ AULAS/Exemplo_Aplicativo/script.js
@@ -0,0 +1,39 @@
+const _listaDeUsuarios = []
+
+function enviar() {
+    const nome = String(document.getElementById('nome').value)
+    const senha = String(document.getElementById('senha').value)
+
+    const obj = {
+        'nome': nome,
+        'senha': senha
+    }
+
+    for(const index in _listaDeUsuarios){
+        const usuario = _listaDeUsuarios[index].nome
+        if(nome == usuario){
+            return console.log("Usuário já existe")
+        }
+    }
+
+    _listaDeUsuarios.push(obj)
+    localStorage.setItem('usuarios', JSON.stringify(_listaDeUsuarios))
+
+    console.log(_listaDeUsuarios)
+}
+
+function deletarUsuario(){
+    const nome = prompt("Digite o nome do usuário a ser deletado:")
+
+    for(const index in _listaDeUsuarios){
+        const usuario = _listaDeUsuarios[index].nome 
+        if(nome == usuario){
+            _listaDeUsuarios.splice(index, 1)
+            console.log("Lista depois de ser deletada:")
+            console.log(_listaDeUsuarios)
+        } else{
+            console.log("Não foi possível achar o usuário.")
+            return
+        }
+    }
+}
